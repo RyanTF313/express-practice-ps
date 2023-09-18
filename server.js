5efa83d385b4838f02418e9b2a22a5932032bc65
RyanTF313 <r.tillman.french@gmail.com>
2023-09-18 10:29:29 -0400

    calculator and magic 8 ball complete

--- server.js
+++ server.js
@@ -19,6 +19,41 @@ app.get("/greeting/:name", function (req, res) {
   res.send(`<h1>Hello ${name}! It is so nice to see ya.</h1>`);
 });
 
+app.get("/tip/:total/:tipPercentage", function (req, res) {
+  const { total, tipPercentage } = req.params;
+  res.send(`<h1>${Math.abs(total / Math.abs(100 / tipPercentage))}</h1>`);
+});
+
+app.get("/magic/:question", function (req, res) {
+  const { question } = req.params;
+  const responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
+  res.send(`
+  <p>${question}</p>
+  <h1>${responses[Math.floor(Math.random() * responses.length)]}</h1>
+  `);
+});
+
 // Tell the app to listen on port 3000
 // for HTTP requests from clients
 app.listen(3005, function () {
